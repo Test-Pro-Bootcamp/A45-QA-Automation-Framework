12e685c0b7752fc9cb44a61d818999e14e934a58
Zbolormaa <bolormaa.zanabaatar@testpro.io>
2023-04-21 16:48:02 -0500

    url change
    homework15

---
+++ src/test/java/HWclass.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class HWclass  extends BaseTest{
+
+    public void testMethod(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+    }
+}
