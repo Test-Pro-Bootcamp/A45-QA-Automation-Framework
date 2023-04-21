7235a44d5e0a190c541910b2ca47c8fc16e285ff
Daria Pavlyuk <daria.pavlyuk@testpro.io>
2023-04-21 17:03:46 -0400

    Homework 16
    Homework16 Registration Navigation

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,30 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework16 {
+    @Test
+    public void registrationNavigation(){
+        //WebDriver driver = new ChromeDriver();
+
+        // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id = 'hel']"));
+
+
+        registrationLink.click();
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(),registrationUrl);
+
+
+    }
+}
