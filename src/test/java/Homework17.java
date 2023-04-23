ee9e19252f76257252588b152a8b7ae8c6f3eaa2
AndreiBryliakov <andrei.bryliakov@testpro.io>
2023-04-23 19:16:30 +0200

    HW17 Andrei Bryliakov

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,59 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework17 {
+    @Test
+    public void addSongToPlaylist(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();//not needed
+        emailField.clear();
+        emailField.sendKeys("andrei.bryliakov@testpro.io");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();//not needed
+        passwordField.clear();
+        passwordField.sendKeys("te$t$tudent1");
+
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+
+        WebElement searchBar = driver.findElement(By.cssSelector("input[type='search']"));
+        searchBar.click();
+        searchBar.clear();
+        searchBar.sendKeys("reactor");
+
+        WebElement viewAllButton = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+        viewAllButton.click();
+
+        WebElement songReactor = driver.findElement(By.xpath("//td[contains(text(), 'Reactor')]"));
+        //WebElement songReactor = driver.findElement(By.cssSelector("tr[class='song-item selected']"));
+        songReactor.click();
+
+        WebElement addToButton = driver.findElement(By.cssSelector("button[class = 'btn-add-to']"));
+        addToButton.click();
+
+        WebElement playlistHW17 = driver.findElement(By.cssSelector("#songResultsWrapper > header > div.song-list-controls > div > section.existing-playlists > ul > li:nth-child(5)"));
+        playlistHW17.click();
+
+        WebElement successMessage = driver.findElement(By.xpath("//div[contains(text(), 'Added 1 song into \"HW17.\"')]"));
+
+        Assert.assertTrue(successMessage.isDisplayed());
+        driver.quit();
+    }
+}
