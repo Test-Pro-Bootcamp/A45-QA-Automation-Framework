879e0366b7f03fecec1f4404ca49d4bb3d89a2af
dl808702 <divina.acorda@testpro.io>
2023-04-23 22:49:17 -0700

    Registration Link

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -13,6 +13,21 @@ public class LoginTests extends BaseTest {
     public static void registrationNavigation(){
         if (System.getProperty("os.name").toLowerCase().contains("win")){
             System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+
+            WebElement regLink = driver.findElement(By.cssSelector("button[type='submit']"));
+            regLink.click();
+
+            //WebElement register = driver.findElement(By.cssSelector(""));
+            String regURL = "https://bbb.testpro.io/registration.php";
+            Assert.assertEquals(driver.getCurrentUrl(),regURL);
+            driver.quit();
+        }
+    }
         }
     }
 
@@ -115,3 +130,4 @@ public class LoginTests extends BaseTest {
         driver.quit();
     }
 }
+
