789da230598f68752d012f48bbfabce9a50ee985
Mengitestpro2023 <Mengi.Gebremedhin@testpro.io>
2023-04-24 13:48:01 -0600

    Create MyBaseTest.java

---
+++ src/test/java/MyBaseTest.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
+
+public class MyBaseTest {
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+    }
+}
+
+
+
+}
