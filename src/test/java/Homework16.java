6e77a83884e9134a5a06de4eced363b1c3600973
EHFoshee <esther.foshee@testpro.io>
2023-04-24 13:31:06 -0400

    Directed to Registration Page

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,4 +1,7 @@
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -6,20 +9,29 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
-    @Test
-    public static void loginEmptyEmailPasswordTest() {
 
-//      Added ChromeOptions argument below to fix websocket error
+public class Homework16 extends BaseTest {
+    @Test
+
+    public void registrationNavigation() throws InterruptedException {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://bbb.testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        WebElement registration = driver.findElement(By.cssSelector("a[id='hel']"));
+        registration.click();
+        Assert.assertEquals(driver.getCurrentUrl(),registrationUrl);
+        Thread.sleep(1000);
         driver.quit();
     }
 }
+
+
