a37219f0f11f322a769677b3e1a2575779ca8482
kcroker721 <kcroker7@gmail.com>
2023-04-24 14:32:49 -0400

    homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -8,18 +10,55 @@ import java.time.Duration;
 
 public class Homework17 extends BaseTest {
     @Test
-    public static void addSongToPlaylist() {
+    public static void addSongToPlaylist() throws InterruptedException {
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        String newSongAdded = "Added 1 Song";
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        navigateToPage();
+        provideEmail("kcroker@testpro.io");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        searchSong("Callaita");
+        clickViewAllBtn();
+        selectFirstSongResult();
+        clickAddToBtn();
+        choosePlaylist();
+        Assert.assertTrue(getNotificationText().contains(newSongAdded));
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    }
+
+    public static void searchSong(String songTitle) throws InterruptedException{
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type='search']"));
+        searchField.sendKeys(songTitle);
+        Thread.sleep(2000);
+    }
+
+    public static void clickViewAllBtn() throws InterruptedException {
+        WebElement viewAllSearchResult = driver.findElement(By.cssSelector("div.results section.songs h1 button"));
+        viewAllSearchResult.click();
+        Thread.sleep(2000);
+    }
+
+    public static void selectFirstSongResult() throws InterruptedException {
+        WebElement firstSongResult = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
+        firstSongResult.click();
+        Thread.sleep(2000);
+    }
+
+    public static void clickAddToBtn() throws InterruptedException {
+        WebElement addToBtn = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addToBtn.click();
+        Thread.sleep(2000);
+    }
+
+    public static void choosePlaylist() throws InterruptedException {
+        WebElement playlistElement = driver.findElement(By.xpath("//section[@id ='songResultsWrapper']//li[contains(text(),'Test Pro Playlist')]"));
+        playlistElement.click();
+        Thread.sleep(2000);
+    }
+
+    public static String getNotificationText(){
+        WebElement notificationElement = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationElement.getText();
     }
 }
