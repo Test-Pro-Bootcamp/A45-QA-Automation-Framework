35f4695956ba9380d1b1761cee687bbf866c9582
AndreiBryliakov <andrei.bryliakov@testpro.io>
2023-04-25 21:49:15 +0200

    Fix

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,50 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+
+    static WebDriver driver;
+
+    static String url = "https://bbb.testpro.io";
+
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+    @BeforeMethod
+    static void setUpBrowser(){
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--disable-notifications","--remote-allow-origins=*","--incognito","--start-maximized");
+    driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.get(url);
+
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();//not needed
+        emailField.clear();
+        emailField.sendKeys("andrei.bryliakov@testpro.io");
+
+    WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();//not needed
+        passwordField.clear();
+        passwordField.sendKeys("te$t$tudent1");
+
+    WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+    @AfterMethod
+    static void shutDown(){
+    driver.quit();
     }
 }
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,40 +1,13 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
-public class Homework18  extends BaseTest{
+public class Homework18 extends BaseTest{
 
     @Test
-    public static void playSong(){
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        emailField.click();//not needed
-        emailField.clear();
-        emailField.sendKeys("andrei.bryliakov@testpro.io");
-
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.click();//not needed
-        passwordField.clear();
-        passwordField.sendKeys("te$t$tudent1");
-
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        submit.click();
-
+    public static void playSong() {
         WebElement nextSongButton = driver.findElement(By.cssSelector("i[data-testid='play-next-btn']"));
         nextSongButton.click();
 
@@ -44,7 +17,5 @@ public class Homework18  extends BaseTest{
         WebElement soundBar = driver.findElement(By.cssSelector("button[data-testid='toggle-visualizer-btn']"));
 
         Assert.assertTrue(soundBar.isDisplayed());
-
-        driver.quit();
     }
 }
