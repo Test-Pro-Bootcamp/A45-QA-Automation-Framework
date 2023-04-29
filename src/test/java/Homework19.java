ea3905c4b9af1bd92e3c51cfa0c58924ccd45bfb
ikeatl <adesinaikeoluwa4@gmail.com>
2023-04-28 17:03:52 -0700

    Created and updated Homework19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+
+public class Homework19 extends BaseTest {
+
+
+    public static void clickPlaylist() {
+        WebElement clickPlaylist = driver.findElement(By.xpath ("//*[@id=\"playlists\"]/ul/li/a[contains(text(), \"My List\")]"));
+        clickPlaylist.click();
+    }
+    public static void createPlaylist() {
+        WebElement createPlaylist = driver.findElement(By.cssSelector ("i[title='Create a new playlist']"));
+        createPlaylist.click();
+        WebElement newPlaylist = driver.findElement(By.cssSelector ("li[data-testid='playlist-context-menu-create-simple']"));
+        newPlaylist.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement newPlaylistInput = driver.findElement(By.cssSelector ("form[class='create'] input[type='text']"));
+        newPlaylistInput.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        newPlaylistInput.sendKeys("My List");
+        newPlaylistInput.sendKeys(Keys.RETURN);
+
+    }
+
+
+    public static void clickRedButton() {
+        WebElement clickRedButton = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
+        clickRedButton.click();
+    }
+    //public static void deletePlaylist() {
+    // WebElement deletePlaylist = driver.findElement(By.cssSelector("button[class='ok']"));
+    // deletePlaylist.click();
+    // Thread.sleep(2000);
+    //}
+
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        Thread.sleep(2000);
+        createPlaylist();
+        Thread.sleep(2000);
+        clickPlaylist();
+        Thread.sleep(10000);
+        clickRedButton();
+        Thread.sleep(2000);
+
+
+
+
+        WebElement deletePlaylistNotif = driver.findElement(By.cssSelector("div.success.show"));
+        Assert.assertEquals(deletePlaylistNotif.getText(), "Deleted playlist \"My List.\"");
+
+
+    }
+
+
+}
