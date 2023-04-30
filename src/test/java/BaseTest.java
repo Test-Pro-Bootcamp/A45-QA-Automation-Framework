fcfd049423cdd4471054b2f2c683c0958f99fa1d
Zbolormaa <bolormaa.zanabaatar@testpro.io>
2023-04-30 18:27:39 -0500

    Homework20
    Implemented homework20.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -12,6 +15,7 @@ import java.util.UUID;
 public class BaseTest {
 
     public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
 
     public static String url = "";
 
@@ -38,7 +42,7 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
         navigateToPage();
     }
@@ -49,17 +53,18 @@ public class BaseTest {
 
     public static void navigateToPage() {
         driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(4));
     }
 
     public static void provideEmail(String email) {
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='email']")));
         emailField.click();//not needed
         emailField.clear();
         emailField.sendKeys(email);
     }
 
     public static void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='password']")));
         passwordField.click();//not needed
         passwordField.clear();
         passwordField.sendKeys(password);
