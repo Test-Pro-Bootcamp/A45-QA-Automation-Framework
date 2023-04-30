7f6091f7176af5ed715b2e54008c421f7764e562
Alexey Zaikov <50819607+zaikoval@users.noreply.github.com>
2023-04-30 13:28:57 -0500

    Update Homework19.java

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -2,10 +2,8 @@ import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest{
 
-
     @Test
     public void deletePlaylist() throws InterruptedException{
-
         String playlistName = "zaikov-HW19";
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
@@ -13,15 +11,6 @@ public class Homework19 extends BaseTest{
         isAvatarDisplayed();
         createPl(playlistName); // verifications are inside functions
         deletePl(playlistName);
-
-
-        //Click the playlist you want to delete.
-        //We should see a red button "x PLAYLIST" on the top right part of the page, and click on it.
-        //Note: If the red button is not displayed, let's create a new playlist and click it.
-        //Verify that the confirmation notification displayed has the text, "Deleted playlist {playlist name}".
-
-
-
     }
 
 }
