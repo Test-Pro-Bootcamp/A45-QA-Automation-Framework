2d3d2e775868a7b3c8f374733ca2bfd4cc8ef042
Zbolormaa <bolormaa.zanabaatar@testpro.io>
2023-04-30 18:05:53 -0500

    Homework19
    Implemented homework19.

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test
+    public void deletePlaylist(){
+        navigateToPage();
+        provideEmail("bolormaa.zanabaatar@testpro.io");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        click(".playlist:nth-child(3)");
+        click(".btn-delete-playlist");
+        Assert.assertTrue(getNotification("div.success.show").contains("Deleted"));
+    }
+
+    public void click(String element) {
+        WebElement submit = driver.findElement(By.cssSelector(element));
+        submit.click();
+    }
+
+    public String getNotification(String element) {
+        WebElement submit = driver.findElement(By.cssSelector(element));
+        return submit.getText();
+    }
+
+}
