15192aaa5902f13a8a85a23639be9094fa883044
MyeshaO <130818751+MyeshaO@users.noreply.github.com>
2023-05-01 21:00:02 -0700

    try1

---
+++ src/test/java/Homework20.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class Homework20 extends BaseTest{
+    @Test
+
+  public static void Homework20(){
+
+        logIn("heavenmayhem@gmail.com","Everything2Me0628!");
+        searchSong("Dark Days");
+        clickViewAll();
+        selectSong();
+        clickAddTo();
+        choosePlaylist();
+        deleteButton();
+
+
+    }
+
+}
