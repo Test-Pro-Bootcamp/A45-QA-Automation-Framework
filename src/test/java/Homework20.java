ed2e4d5826e6459ecaedf1ac31e65b632fd7b93e
kcroker721 <kcroker7@gmail.com>
2023-05-02 17:57:09 -0400

    homework 20 kcroker

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -1,25 +1,56 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class Homework20 extends BaseTest {
-    @Test
-    public static void LoginEmptyEmailPasswordTest() {
 
-//      Added ChromeOptions argument below to fix websocket error
+    @Parameters("baseUrl")
+    @Test
+    public static void launchBrowser(String baseUrl) throws InterruptedException {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.get(baseUrl);
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl);
+
+        String email = "kcroker@testpro.io";
+        String password = "te$t$tudent";
+
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.sendKeys(email);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.sendKeys(password);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement emptyPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        emptyPlaylist.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deletePlaylist.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.quit();
+
+
     }
+
 }
