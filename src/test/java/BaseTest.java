0221b39b2576e168831cc1ed198d02c0ee3ffc5c
ikeatl <adesinaikeoluwa4@gmail.com>
2023-05-01 23:53:38 -0700

    refactored using explicit wait on hw 17,18,19

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import java.time.Duration;
 import java.util.UUID;
 
 public class BaseTest {
+    static WebDriverWait wait;
 
     public static WebDriver driver = null;
 
@@ -38,6 +40,7 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
         navigateToPage();
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,36 +8,32 @@ import org.testng.annotations.Test;
 public class Homework17 extends BaseTest {
 
     @Test
-
     public void addSongToPlaylist() throws InterruptedException {
-
         navigateToPage();
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
         clickSubmit();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[class='home active']")));
 
         WebElement songSearch = driver.findElement(By.cssSelector("input[type='search']"));
         songSearch.sendKeys("Pluto");
 
         WebElement viewAll = driver.findElement(By.cssSelector("div.results h1 > button"));
         viewAll.click();
-        Thread.sleep(2000);
 
         WebElement firstSong = driver.findElement(By.cssSelector("section#songResultsWrapper td.title"));
         firstSong.click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("section#songResultsWrapper td.title")));
 
         WebElement addToButton = driver.findElement(By.cssSelector("button.btn-add-to"));
         addToButton.click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn-add-to")));
 
-        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'julia play')]"));
-        playlist.click();
-        Thread.sleep(2000);
+        WebElement addToPlaylist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'julia play')]"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'julia play')]")));
+        addToPlaylist.click();
 
         WebElement notif = driver.findElement(By.cssSelector("div.success.show"));
         Assert.assertEquals(notif.getText(), "Added 1 song into \"julia play.\"");
-
     }
 }
