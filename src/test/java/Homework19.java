a063d1dc538d00646351d4d8614867b3e15a1c03
RomeoFurnic <romeo.furnic@testpro.io>
2023-05-03 09:41:06 -0700

    Homework20
    Fixed

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -38,7 +38,7 @@ public class Homework19 extends BaseTest{
         newPlaylistName.sendKeys("NewPLaylist" + Keys.ENTER);
         //Thread.sleep(5000); //This Thread.sleep is needed for this test. In this 5 seconds one successful notification disappears so the other one can appear without interfering.
 
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='success show']")));
+        //wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='success show']")));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//section[@id='playlists']//li[@class='playlist playlist']//a[position()]"))).click();
         //WebElement playlist = driver.findElement(By.xpath("//section[@id='playlists']//li[@class='playlist playlist']//a[position()]"));
         //playlist.click();
@@ -49,10 +49,9 @@ public class Homework19 extends BaseTest{
         //deletePlaylistBtn.click();
         //Thread.sleep(500);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='success show']")));
-        WebElement succesShowNotif = driver.findElement(By.xpath("//div[@class='success show']"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Deleted playlist \"NewPLaylist.\"')]")));
+        WebElement succesShowNotif = driver.findElement(By.xpath("//div[contains(text(),'Deleted playlist \"NewPLaylist.\"')]"));
         Assert.assertEquals(succesShowNotif.getText(), "Deleted playlist \"NewPLaylist.\"");
 
-        //Test needs to be runned from the Terminal using Gradle clean test. This way Parameters setted in the TestNG.xml will work.
     }
 }
