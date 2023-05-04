8b5427324eeb73f2e964b8fd06e9f449115b64d3
mattannand <matthew.annand@testpro.io>
2023-05-03 20:49:25 -0700

    Create Homework16.java

---
+++ Homework16.java
@@ -0,0 +1,19 @@
+public class Homework16 extends BaseTest {
+    @Test
+    public static void registrationNavigation() {
+
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
+        registrationLink.click();
+
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
