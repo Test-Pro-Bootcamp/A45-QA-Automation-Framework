294a231aaf6992350acb8232f99ac947f0e8d13c
Daria Pavlyuk <daria.pavlyuk@testpro.io>
2023-05-05 16:01:54 -0400

    Homework21RenamePLaylistupdate

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -15,10 +15,13 @@ public class Homework21 extends BaseTest {
             clickSubmit();
             doubleClickPlaylist();
             enterNewPlaylistName();
-            Assert.assertTrue(doesPlaylistExist());
+            Assert.assertEquals(newPlaylistName,getPlaylistName());
+
         }
 
-        public void doubleClickPlaylist() {
+
+
+    public void doubleClickPlaylist() {
             WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));//added 1 closing parenthesis
 //WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(//*[@id="playlists"]/ul/li[3]/a)));
             actions.doubleClick(playlistElement).perform();
@@ -31,11 +34,17 @@ public void enterNewPlaylistName(){
     playlistInputField.sendKeys(Keys.ENTER);
 
     }
-    public boolean doesPlaylistExist() {
-        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='"+newPlaylistName+"']")));
+    public String getPlaylistName() {
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='" + newPlaylistName + "']")));
         //WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated (By.xpath("//a*[@id="playlists"]/ul/li[3]/input)))
-        return playlistElement.isDisplayed();
-
+        return playlistElement.getText();
     }
-}
+        //public String getPlaylistName(){
+            //WebElement playlistElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+            //return playlistElement.getText();
+
+        }
+
+
+
 
