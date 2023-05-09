5fa83af765d3ebabab45f7e438a7bfd5ff5199bc
kcroker721 <kcroker7@gmail.com>
2023-05-09 15:26:38 -0400

    homework23 kcroker

---
+++ src/test/java/Homework23.java
@@ -0,0 +1,21 @@
+import Pages.HomePage;
+import Pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class Homework23 extends BaseTest {
+    @Test
+    public void renamePlaylist() {
+
+        String playlistName = "kcroker playlist homework22";
+
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.login();
+
+        homePage.doubleClickPlaylist();
+        homePage.enterNewPlaylistName(playlistName);
+
+    }
+
+}
