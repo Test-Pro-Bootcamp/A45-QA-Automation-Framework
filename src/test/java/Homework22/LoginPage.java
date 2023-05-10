fd67ecd4f140c12205c12cf03ab7a9574c570371
MyeshaO <130818751+MyeshaO@users.noreply.github.com>
2023-05-09 17:56:56 -0700

    1go

--- src/test/java/Homework22/LoginPage.java
+++ src/test/java/Homework22/LoginPage.java
@@ -1,25 +1,32 @@
 package Homework22;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
 
     public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
     }
-    By emailField = By.cssSelector("input[type='email']");
-    By passwordField = By.cssSelector("input[type='password']");
-    By submitButton = By.cssSelector("button[type='submit']");
+//    By emailField = By.cssSelector("input[type='email']");
+//    By passwordField = By.cssSelector("input[type='password']");
+//    By submitButton = By.cssSelector("button[type='submit']");
+    @FindBy(css = "button[type='submit']")
+    WebElement submitButton;
+    @FindBy(css = "input[type='password']")
+    WebElement passwordField;
+   @FindBy(css = "input[type='email']")
+   WebElement emailField;
 
     public void provideEmail(String email){
-        findElement(emailField).sendKeys(email);
+        emailField.sendKeys(email);
     }
     public void providePassword(String password){
-        findElement(passwordField).sendKeys(password);
+        passwordField.sendKeys(password);
     }
     public void setSubmitButton(){
-        findElement(submitButton).click();
+        submitButton.click();
     }
     public void logIn(){
         provideEmail("heavenmayhem@gmail.com");
---
+++ src/test/java/Homework23.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+
+public class Homework23 extends BaseTest{
+    @Test
+    @FindBy
+
+}
