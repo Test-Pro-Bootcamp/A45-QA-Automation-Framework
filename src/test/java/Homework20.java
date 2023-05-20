62abca52ecd97fc355de75ff14ee198eaec80313
dj <deimante.juskyte@testpro.io>
2023-05-20 10:20:01 -0400

    Update Homework20.java

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -8,6 +8,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Homework20 {
