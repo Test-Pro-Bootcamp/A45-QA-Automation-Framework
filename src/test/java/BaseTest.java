ae1c1af0eb79c5f285c1c54c47dc3aae176e6de0
PavelFurnic <132508015+PavelFurnic@users.noreply.github.com>
2023-05-22 16:59:04 -0700

    Homework21

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -13,10 +14,12 @@ import org.testng.annotations.Parameters;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
+
 public class BaseTest {
     public static WebDriver driver = null;
     public static String url = "";
-    public static WebDriverWait wait;
+    public static WebDriverWait wait = null;
+    public static Actions actions = null;
     @BeforeSuite
     static void setupClass() {
 
@@ -31,8 +34,9 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseUrl;
         driver.get(url);
         navigateToPage();
---
+++ src/test/java/HomeWork21.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class HomeWork21 extends BaseTest{
+    @Test
+    public void renamePlayList () throws InterruptedException{
+
+        provideEmail("pavel.furnic@testpro.io");
+        providePassword("Test123456789@");
+        clickSubmit();
+
+
+        WebElement selectPlayList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//section[@id='playlists']//li[@class='playlist playlist']//a[position()]")));
+        actions.doubleClick(selectPlayList).perform();
+
+        WebElement playlistInputName =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='name']")));
+        playlistInputName.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.BACK_SPACE));
+        playlistInputName.sendKeys("New Play List");
+        playlistInputName.sendKeys(Keys.ENTER);
+
+        WebElement playListElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='success show']")));
+        assert  playListElement.isDisplayed();
+        
+    }
+
+}
