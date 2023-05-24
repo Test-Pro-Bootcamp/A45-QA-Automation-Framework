f13841f572a2e43ab0ebf71fb59ac91a38f68bbf
Not-Nerwell <anton.prymak@testpro.io>
2023-05-24 07:16:35 +0300

    HW19
    ----

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -20,24 +22,17 @@ public class BaseTest {
 
     public static String url = "";
 
-    @DataProvider(name = "IncorrectLoginData")
-    public static Object [][] getDataFromDataProviders() {
-
-        return new Object[][] {
-                {"invalid@mail.com", "invalidPass"},
-                {"anton.prymak@testpro.io", ""},
-                {"", ""}
-        };
-    }
-
     @BeforeMethod
-    @Parameters({"BaseURL"})
+    @Parameters ({"BaseURL"})
     public void launchBrowser(String BaseURL) {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
+
         driver = new ChromeDriver(options); // Assigning the driver instance to the static driver variable
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
+        driver.get(url);
         navigateToPage();
     }
 
@@ -67,40 +62,5 @@ public class BaseTest {
     public static void clickSubmit() {
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
-    }
-
-    protected String getNotificationText() {
-        WebElement notification = driver.findElement(By.xpath("//div[@class='success show']"));
-        return notification.getText();
-    }
-
-    protected String removePlaylist(Integer ordNum) {
-
-        WebElement certainPlaylist = driver.findElement(By.xpath(String.format("//li[@class='playlist playlist'][%d]/a",
-                                                                                ordNum)));
-        String plName = certainPlaylist.getText();
-        certainPlaylist.getText();
-        certainPlaylist.click();
-        WebElement delButton = driver.findElement(By.xpath("//button[@class='del btn-delete-playlist']"));
-        delButton.click();
-        try {
-            WebElement okBtn = driver.findElement(By.xpath( "//button[@class='ok']"));
-            okBtn.click();
-        }
-        catch (NoSuchElementException e){
-        }
-        return plName;
-    }
-
-    protected void createPlaylist(String playlistName) throws InterruptedException {
-        WebElement plusButton = driver.findElement(By.xpath("//i[@data-testid='sidebar-create-playlist-btn']"));
-        plusButton.click();
-
-        WebElement newPlaylist = driver.findElement(By.xpath("//li[@data-testid='playlist-context-menu-create-simple']"));
-        newPlaylist.click();
-
-        WebElement toSave = driver.findElement(By.xpath("//input[@name='name']"));
-        toSave.sendKeys(playlistName);
-        toSave.submit();
     }
 }
--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,21 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework19 extends BaseTest {
+public class Homework19 extends BaseTest{
 
     @Test
-    public void deletePlaylist() throws InterruptedException {
-
-        String playlistName = "Tony2";
+    public void deletePlaylist () throws InterruptedException {
 
         provideEmail("anton.prymak@testpro.io");
         providePassword("te$t$tudent");
         clickSubmit();
-        Thread.sleep(2000);
-        String deletedPlaylist = removePlaylist(1);
-        String notificationText = getNotificationText();
-        Assert.assertEquals(notificationText,
-                String.format("Deleted playlist \"%s.\"", deletedPlaylist));
+
+        WebElement selectPlayList = driver.findElement(By.xpath("//li[@class = 'playlist playlist']"));
+        selectPlayList.click();
+        Thread.sleep(200);
+
+        WebElement deletePlaylistButton = driver.findElement(By.xpath("//button[@class ='del btn-delete-playlist']"));
+        deletePlaylistButton.click();
+        Thread.sleep(200);
+
+        WebElement confirmationMessage = driver.findElement(By.xpath("//div[@class='success show']"));
+        Assert.assertTrue(confirmationMessage.isDisplayed());
+
+
     }
 
 }
