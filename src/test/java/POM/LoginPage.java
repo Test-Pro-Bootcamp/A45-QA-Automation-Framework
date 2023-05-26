c72ae8eb087e5e267b85b2858ac8655e7dc3fa8f
PavelFurnic <132508015+PavelFurnic@users.noreply.github.com>
2023-05-26 15:04:04 -0700

    HomeWork23

--- src/test/java/POM/LoginPage.java
+++ src/test/java/POM/LoginPage.java
@@ -4,27 +4,33 @@ package POM;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasePage {
-    By clickSubmit = By.xpath("//button[@type='submit']");
-    By provideEmail = By.xpath("//input[@type='email']");
-    By providePassword = By.xpath("//input[@type='password']");
+    @FindBy (xpath="//button[@type='submit']")
+    WebElement clickSubmit;
+    @FindBy (xpath ="//input[@type='email']")
+    WebElement provideEmail;
+    @FindBy (xpath ="//input[@type='password']")
+    WebElement providePassword;
 
 
     public LoginPage( WebDriver givenDriver){
         super(givenDriver);
     }
-    public void clickSubmit (){
-        driver.findElement(clickSubmit).click();
+    public LoginPage clickSubmit () {
+        clickSubmit.click();
+        return this;
     }
-    public void provideEmail( String email){
-        WebElement emailElement = driver.findElement(provideEmail);
-        emailElement.sendKeys(email);
+    public LoginPage provideEmail( String email){
+        provideEmail.sendKeys(email);
+        return this;
+
     }
-    public void providePassword(String password){
-        WebElement passwordElement = driver.findElement(providePassword);
-        passwordElement.sendKeys(password);
+    public LoginPage providePassword(String password){
+        providePassword.sendKeys(password);
+        return this;
     }
     public void login(){
         provideEmail("pavel.furnic@testpro.io");
