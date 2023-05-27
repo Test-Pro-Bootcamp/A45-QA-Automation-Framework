5680d2f5ee559d536faea1fe7a4ea50c78fad198
Not-Nerwell <anton.prymak@testpro.io>
2023-05-28 02:20:26 +0300

    Homework20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,18 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BaseTest {
-    static WebDriverWait wait;
+
     public static WebDriver driver = null;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -31,8 +30,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
 
         driver = new ChromeDriver(options); // Assigning the driver instance to the static driver variable
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //explicit wait
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         url = BaseURL;
         driver.get(url);
         navigateToPage();
@@ -49,8 +47,7 @@ public class BaseTest {
 
     public static void provideEmail(String email) {
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='email']"))).click();
-        //emailField.click(); // not needed, the last part of previous line works for it
+        emailField.click();
         emailField.clear();
         emailField.sendKeys(email);
     }
@@ -65,15 +62,5 @@ public class BaseTest {
     public static void clickSubmit() {
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
-    }
-
-    public void selectPlayList() {
-        WebElement selectPlayList = driver.findElement(By.xpath("//li[@class = 'playlist playlist']"));
-        selectPlayList.click();
-    }
-
-    public void deletePlaylistButton() {
-        WebElement deletePlaylistButton = driver.findElement(By.xpath("//button[@class ='del btn-delete-playlist']"));
-        deletePlaylistButton.click();
     }
 }
