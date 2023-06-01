7254dc20ed847f39ca219075fd787495080b2bdc
dasha <132013777+Dashaari@users.noreply.github.com>
2023-05-31 23:11:31 -0400

    HW27

--- src/test/java/StepDefinition/MyStepdefs.java
+++ src/test/java/StepDefinition/MyStepdefs.java
@@ -1,23 +1,49 @@
 package StepDefinition;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 
 
 public class LoginStepDefinition {
-    @Given("You open login page")
-    public void youOpenLoginPage() {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+@Before
+
+    @Given("You open browser")
+    public void youOpenBrowser() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--disable-notifications");
+    options.addArguments("--remote-allow-origins=*");
+    driver = new ChromeDriver(options);
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+}
+@After
+public void closeBrowser() {driver.quit();}
+    @And("You open login page")
+    public void youOpenLoginPage() {driver.get("https://bbb.testpro.io/");
     }
 
-    @When("I enter email")
-    public void iEnterEmail() {
+    @When("I enter email {string}")
+    public void iEnterEmail(String arg0) {
     }
 
-    @And("I enter password")
-    public void iEnterPassword() {
+
+    @And("I enter password {string}")
+    public void iEnterPassword(String arg0) {
     }
 
     @And("I submit")
@@ -28,11 +54,7 @@ public class LoginStepDefinition {
     public void iAmLoginIn() {
     }
 
-    @When("I enter email {string}")
-    public void iEnterEmail(String arg0) {
-    }
-
-    @And("I enter password {string}")
-    public void iEnterPassword(String arg0) {
+    @And("I open Login Page")
+    public void iOpenLoginPage() {
     }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,7 +1,8 @@
 Feature: Login feature
 
   Scenario: Login success
-    Given You open login page
+    Given You open browser
+    And I open Login Page
     When I enter email "demo@class.com"
     And I enter password "te$t$tudent"
     And I submit
