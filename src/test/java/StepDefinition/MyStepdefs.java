cc9f59830f8cbb3865bb189dcddbb7b54b6866be
dasha <132013777+Dashaari@users.noreply.github.com>
2023-06-01 15:50:44 -0400

    HW27

--- src/test/java/StepDefinition/MyStepdefs.java
+++ src/test/java/StepDefinition/MyStepdefs.java
@@ -7,54 +7,66 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
 
-public class LoginStepDefinition {
+public class MyStepdefs {
 
     WebDriver driver;
     WebDriverWait wait;
 
-@Before
+    @Before
 
     @Given("You open browser")
     public void youOpenBrowser() {
-    WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--disable-notifications");
-    options.addArguments("--remote-allow-origins=*");
-    driver = new ChromeDriver(options);
-    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-}
-@After
-public void closeBrowser() {driver.quit();}
-    @And("You open login page")
-    public void youOpenLoginPage() {driver.get("https://bbb.testpro.io/");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    @When("I enter email {string}")
-    public void iEnterEmail(String arg0) {
-    }
-
-
-    @And("I enter password {string}")
-    public void iEnterPassword(String arg0) {
-    }
-
-    @And("I submit")
-    public void iSubmit() {
-    }
-
-    @Then("I am login in")
-    public void iAmLoginIn() {
+    @After
+    public void closeBrowser() {
+        driver.quit();
     }
 
     @And("I open Login Page")
-    public void iOpenLoginPage() {
+    public void IOpenLoginPage() {
+        driver.get("https://bbb.testpro.io/");
     }
+
+    @When("I enter email {string}")
+    public void iEnterEmail(String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys(email);
+    }
+
+    @And("I enter password {string}")
+    public void iEnterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']"))).sendKeys(password);
+    }
+
+    @And("I submit")
+    public void clickSubmit() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='submit']"))).click();
+    }
+
+    @Then("I am login in")
+    public void userIsLoginIn() {
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+
+    }
+
 }
+
+
+
