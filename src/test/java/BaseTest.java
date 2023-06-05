75d4a8887057f2cd4ee8135122a5402f75c343e5
Deepthi Raman <deepthi.raman@testpro.io>
2023-06-05 00:34:04 -0700

    Submitting Homework23

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -42,6 +42,8 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
         url = BaseURL;
         navigateToPage();
     }
