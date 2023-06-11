192997fd5417f0ae43e15c5c8db8f20129d51bb4
EHFoshee <esther.foshee@testpro.io>
2023-06-11 08:00:50 -0400

    HW18

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+
+    @Test
+    public static void playSong() throws InterruptedException {
+        provideEmail();
+        providePassword();
+        logInButton();
+
+        WebElement allSongs = driver.findElement(By.xpath("//a[@class='songs']"));
+        allSongs.click();
+
+        WebElement firstSong = driver.findElement(By.xpath("//td[contains(text(),'Waiting')]"));
+        firstSong.click();
+        Thread.sleep(2000);
+
+        WebElement playNextSong = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        playNextSong.click();
+
+        WebElement playSong = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        playSong.click();
+
+        WebElement soundBar = driver.findElement(By.xpath("//div[@class='bars']"));
+        Thread.sleep(2000);
+        soundBar.isDisplayed();
+        }}
+
+
