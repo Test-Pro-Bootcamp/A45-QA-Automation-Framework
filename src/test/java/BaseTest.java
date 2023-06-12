6e9bc36231294ed5e5531502916bd49e88874525
KristinaMatskaylo <kristina.matskaylo@testpro.io>
2023-06-12 17:25:47 -0400

    Add chromeoptions arguments for Jenkins

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -67,6 +67,8 @@ public class BaseTest {
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--window-size=1920,1080");
                 return driver = new ChromeDriver(chromeOptions);
         }
     }
