5b764b49d4e6b439fe8514e7214ea301342b4826
Viaches911 <131311110+Viaches911@users.noreply.github.com>
2023-07-10 17:46:13 -0400

    Summary

--- src/test/java/HomeWork20.java
+++ src/test/java/HomeWork20.java
@@ -1,2 +1,26 @@
-public class HomeWork20 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeWork20 extends BaseTest {
+        // Remake of Homework19 (Homework19 itself was left the same)
+        @Test
+        public void deletePlaylist() {
+            provideEmail("viacheslav.dzhilov@testpro.io");
+            providePassword("te$t$tudent");
+            clickSubmit();
+
+            WebElement selectPlayList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[@class='playlist playlist']")));
+            selectPlayList.click();
+//delete playlist
+            WebElement deletePlaylistButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='del btn-delete-playlist']")));
+            deletePlaylistButton.click();
+//confirmation
+            WebElement confirmationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='success show']")));
+            Assert.assertTrue(confirmationMessage.isDisplayed());
+
+   }
+
 }
