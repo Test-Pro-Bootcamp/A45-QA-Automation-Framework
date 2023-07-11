4b5144f21f21fbb411d22a8719e7bc7d526acea4
EHFoshee <esther.foshee@testpro.io>
2023-07-11 16:19:02 -0400

    homework23

--- src/test/java/Homework23LoginTests.java
+++ src/test/java/Homework23LoginTests.java
@@ -8,17 +8,23 @@ public class Homework23LoginTests extends BaseTest {
 
     @Test (dataProvider = "IncorrectData", dataProviderClass = BaseTest.class)
     public void loginInvalidEmailInvalidPasswordTest(String email, String incorrectPassword) throws InterruptedException {
-        provideIncorrectEmail(email);
-        provideIncorrectPassword(incorrectPassword);
-        logInButton();
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);provideIncorrectEmail(email);
+
+        loginPage.provideIncorrectPassword(incorrectPassword);
+        //calling the object of the class (loginPage) not the class itself (LoginPage)
+        loginPage.clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
     @Test
     public void validLogInCredentials()  {
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
+
         loginPage.login();
-        homePage.assertAvatar();
+        Assert.assertTrue(homePage.assertAvatar());
+
+        //homePage.assertAvatar();
 
     }
 }
--- src/test/java/pagefactory/LoginPage.java
+++ src/test/java/pagefactory/LoginPage.java
@@ -1,37 +1,54 @@
 package pagefactory;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class LoginPage extends BasePage{
-    public LoginPage (WebDriver givenDriver){
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
     }
+
     @FindBy(css = "input[type='email']")
     private WebElement emailField;
-    @FindBy (css = "input[Type='password']")
+    @FindBy(css = "input[Type='password']")
     private WebElement passwordField;
-    @FindBy (css = "button[Type='submit'")
+    @FindBy(css = "button[Type='submit'")
     private WebElement submitBtn;
-    public LoginPage provideEmail (String email){
+
+    public LoginPage provideEmail(String email) {
         emailField.sendKeys(email);
         return this;
     }
-    public LoginPage providePassword (String password){
+
+    public LoginPage providePassword(String password) {
         passwordField.sendKeys(password);
         return this;
     }
-    public LoginPage clickSubmit(){
+
+    public LoginPage clickSubmit() {
         submitBtn.click();
         return this;
     }
+
     public LoginPage login() {
         provideEmail("esther.martinez32@gmail.com");
         providePassword("Proverbs99!");
         clickSubmit();
         return this;
-    }}
+    }
+
+    public LoginPage provideIncorrectPassword(String incorrectPassword) {
+        WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='password']")));
+        //driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(incorrectPassword);
+        return this;
+    }
+}
 
 
 
