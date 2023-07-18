2ba72c12f188ce94098e5dbce636b09c39ae3e67
ikeatl <adesinaikeoluwa4@gmail.com>
2023-07-18 16:59:35 -0700

    Homework 23 finished

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,8 +10,24 @@ public class HomePage extends BasePage {
         super(givenDriver);
     }
     By userAvatarIcon = By.cssSelector("img.avatar");
+    By firstPlaylist = By.cssSelector(".playlist:nth-child(3)");
+    By playlistNameField = By.cssSelector("[name='name']");
     public WebElement getUserAvatar () {
         return findElement(userAvatarIcon);
     }
 
+    public HomePage doubleClickChoosePlaylist() {
+        doubleClick(firstPlaylist);
+        return this;
+    }
+    public void enterNewPlaylistName(String playlistName) {
+        findElement(playlistNameField).sendKeys(playlistName);
+        findElement(playlistNameField).sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.BACK_SPACE));
+        findElement(playlistNameField).sendKeys(playlistName);
+        findElement(playlistNameField).sendKeys(Keys.ENTER);
+    }
+    public boolean doesPlaylistExist(String playlistName) {
+        By newPlaylist = By.xpath("//a[text()='"+playlistName+"']");
+        return findElement(newPlaylist).isDisplayed();
+    }
 }
