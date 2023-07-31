4e4d85281d24aa626929f0e390bf825e64e67e66
EHFoshee <esther.foshee@testpro.io>
2023-07-31 07:33:07 -0400

    hw24update

--- src/test/java/Homework24LoginTests.java
+++ src/test/java/Homework24LoginTests.java
@@ -3,22 +3,29 @@ import org.testng.annotations.Test;
 import pagefactory.HomePage;
 import pagefactory.LoginPage;
 
-public class LoginTests extends BaseTest {
+public class Homework24LoginTests extends BaseTest {
 
-
+//add this to SmokeTestxml for hw24
     @Test (dataProvider = "IncorrectData", dataProviderClass = BaseTest.class)
     public void loginInvalidEmailInvalidPasswordTest(String email, String incorrectPassword) throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
         provideIncorrectEmail(email);
-        provideIncorrectPassword(incorrectPassword);
-        logInButton();
+
+        loginPage.provideIncorrectPassword(incorrectPassword);
+        //calling the object of the class (loginPage) not the class itself (LoginPage)
+        loginPage.clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
     @Test
-    public void ValidLogInCredentials() throws InterruptedException {
+    public void validLogInCredentials()  {
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
+
         loginPage.login();
-        homePage.assertAvatar();
+        Assert.assertTrue(homePage.assertAvatar());
+
+        //homePage.assertAvatar();
 
     }
 }
