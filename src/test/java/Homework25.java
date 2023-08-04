c6010eb9b386aedd44d4c520eefc88cfbd5e6052
ikeatl <adesinaikeoluwa4@gmail.com>
2023-08-04 01:43:17 -0700

    completed homework25 automation

---
+++ src/test/java/Homework25.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.PlaylistsPage;
+public class Homework25 extends BaseTest {
+    @Test
+    public void renamePlaylistTest() {
+        
+    }
+}
