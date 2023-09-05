07885c07f41db674a68ccbcd40fb2ea5ee2346f8
EHFoshee <esther.foshee@testpro.io>
2023-09-05 18:44:55 -0400

    27

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -1,5 +1,8 @@
+import io.cucumber.testing.CucumberOptions;
 import io.cucumber.testing.AbstractTestNGCucumberTests;
 
+@CucumberOptions(
+        features = {"src/test/resources/features/Login.feature"})
 
 public class CucumberRunner {
 }
