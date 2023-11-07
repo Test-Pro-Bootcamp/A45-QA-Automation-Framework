7ee8277d82d8e2c38111e023051380861aed171c
EHFoshee <esther.foshee@testpro.io>
2023-11-07 14:22:41 -0500

    finished first phase of updating pw

--- src/test/java/InternshipUpdatePassword.java
+++ src/test/java/InternshipUpdatePassword.java
@@ -9,13 +9,14 @@ import pom.InternshipProfilePreferences;
 
 public class InternshipUpdatePassword extends BaseTest {
 
-    @Test
-    public void updatePassword() throws InterruptedException {
+    @Test (enabled = true, priority = 1, description = "update password")
+    public void updatePassword() {
         InternshipLoginPage internshipLoginPage = new InternshipLoginPage(driver);
         InternshipProfilePreferences internshipProfilePreferences = new InternshipProfilePreferences(driver);
         InternshipHomePage internshipHomePage = new InternshipHomePage(driver);
-        //Update password
+
         internshipLoginPage.loginInitialPassword();
+        Assert.assertTrue(internshipHomePage.assertAvatar().isDisplayed());
 
         internshipProfilePreferences.openUserProfilePage();
         internshipProfilePreferences.provideCurrentPassword("Proverbs99!");
@@ -30,42 +31,63 @@ public class InternshipUpdatePassword extends BaseTest {
         String expectedSuccessMessage = "Profile updated.";
         //***calling the object of InternshipProfilePreferences (lower case i) instead of the class
         Assert.assertEquals(internshipProfilePreferences.getPopUpSuccessMessage(), expectedSuccessMessage);
-        //***Also assert that new password is saved in database in encrypted format***
-        //Logout
+//        ***Also assert that new password is saved in database in encrypted format***
+    }
+    @Test (enabled = true, priority = 2, description = "login with updated password")
+    void loginWithUpdatedPassword() throws InterruptedException{
+        InternshipLoginPage internshipLoginPage = new InternshipLoginPage(driver);
+        InternshipProfilePreferences internshipProfilePreferences = new InternshipProfilePreferences(driver);
+        InternshipHomePage internshipHomePage = new InternshipHomePage(driver);
 
-        //Thread.sleep(5000);
-        // waiting to fix the wait problem and then figure out how to use a different pw and string
-        internshipHomePage.logout();
-        //verify logged out
-        Assert.assertTrue(internshipLoginPage.submitBtn().isDisplayed());
-        //Login with newly saved password
         internshipLoginPage.loginNewPassword();
-        //verify logged in
+        //Thread.sleep(3000);
         Assert.assertTrue(internshipHomePage.assertAvatar().isDisplayed());
-        //Logout
-        internshipHomePage.logout();
-        //verify logged out
-        Assert.assertTrue(internshipLoginPage.submitBtn().isDisplayed());
-        //Login with old password
-        internshipLoginPage.loginInitialPassword();
-        //Verify not logged in
-        Assert.assertTrue(internshipLoginPage.submitBtn().isDisplayed());
-//
-//        //Clean up - Change password to former password so that test will work again as coded
-//        internshipLoginPage.loginNewPassword();
-//        internshipProfilePreferences.openUserProfilePage();
-//        internshipProfilePreferences.provideCurrentPassword("IwillSing99!");
-//        internshipProfilePreferences.setNewPassword("Proverbs99!");
-//        internshipProfilePreferences.saveProfile();
-//        //Verify password updated
-//        Assert.assertTrue(internshipProfilePreferences.profileUpdateSuccess().equals("Profile updated"));
-//
+        }
+   @Test (enabled = true, priority = 3, description = "login old password")
+   void loginWithOldPassword(){
+       InternshipLoginPage internshipLoginPage = new InternshipLoginPage(driver);
+       InternshipProfilePreferences internshipProfilePreferences = new InternshipProfilePreferences(driver);
+       InternshipHomePage internshipHomePage = new InternshipHomePage(driver);
+
+       internshipLoginPage.loginInitialPassword();
+       Assert.assertTrue(internshipLoginPage.submitBtn().isDisplayed());
+   }
+   @Test (enabled = true, priority = 4, description = "change back to initial password")
+   void changeBackToInitialPassword(){
+       InternshipLoginPage internshipLoginPage = new InternshipLoginPage(driver);
+       InternshipProfilePreferences internshipProfilePreferences = new InternshipProfilePreferences(driver);
+       InternshipHomePage internshipHomePage = new InternshipHomePage(driver);
+
+       internshipLoginPage.loginNewPassword();
+       internshipProfilePreferences.openUserProfilePage();
+       internshipProfilePreferences.provideCurrentPassword("IwillSing99!");
+       internshipProfilePreferences.setNewPassword("Proverbs99!");
+       internshipProfilePreferences.saveProfile();
+       //String expectedSuccessMessage = "Profile updated.";
+       Assert.assertTrue(internshipProfilePreferences.getPopUpSuccessMessage().equals("Profile updated."));
+      // Assert.assertEquals(internshipProfilePreferences.getPopUpSuccessMessage(), expectedSuccessMessage);
+   }
+
         //***figure out how to use the data provider to pom page (also move from basetest) instead of hard coding***
 
+//to figure out how to use a string later
+//        String newName = profilePreferences.getRandomString();
+//
+//        loginPage.login();
+////        Thread.sleep(3000);
+//        profilePreferences.openUserProfilePage();
+//        profilePreferences.setCurrentPassword("Proverbs99!");
+//        profilePreferences.setNewName(newName);
+//        profilePreferences.provideTestEmail("esther.martinez32@gmail.com");
+//        //    profilePreferences.provideTestPassword("te$t$tudent");
+//
+//
+//        profilePreferences.saveProfile();
+//        profilePreferences.getNewProfileName();
+//        Assert.assertEquals(newName, profilePreferences.getNewProfileName());
 
 
 
-
-}}
+}
 
 
